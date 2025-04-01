372b0daa99917609fcd3771685ca6c0850e02d59
PWSdelta <1550443+PWSdelta@users.noreply.github.com>
2025-03-31 22:06:40 -0500

    ports ports

--- app.py
+++ app.py
@@ -582,6 +582,5 @@ def hello_world():
 
 if __name__ == '__main__':
     # Get port from environment variable or use 5000 as default
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run()
 
