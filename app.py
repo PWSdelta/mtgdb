789e468af67b4d5b2e166895f23be872df61b1a4
PWSdelta <1550443+PWSdelta@users.noreply.github.com>
2025-04-04 22:59:56 -0500

    just spot price for hero card on home page

--- app.py
+++ app.py
@@ -1174,8 +1174,8 @@ def hello_world():
         #     thread = threading.Thread(target=delta_price_workflow, args=(hero_card,))
         #     thread.start()
         #
-        enrichment_card = fetch_random_card_from_db()
-        delta_price_workflow(enrichment_card)
+        # enrichment_card = fetch_random_card_from_db()
+        # delta_price_workflow(enrichment_card)
 
         random_cards = session.query(
             CardDetails.id,
